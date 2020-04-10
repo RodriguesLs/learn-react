7ad9bc6942ec97e7f6c99152d8748ec0c032e75f
Rodrigues <felipe.rodrigues@devires.com.br>
2020-04-10 09:35:14 -0300

    learn(component): create first component

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <h1>First component with React</h1>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import FirstComponent from './components/FirstComponent';
 
 const root = document.getElementById('root');
 ReactDom.render(<h1>Hello, React!</h1>, root);
+ReactDom.render(
+  <div>
+    <FirstComponent />
+  </div>
+  , root);
