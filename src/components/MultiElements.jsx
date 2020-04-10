65a8c6bcdc38a13f51fc9b44cd7191dc2e047383
Rodrigues <felipe.rodrigues@devires.com.br>
2020-04-10 10:16:00 -0300

    learn(exports): how to export multiples elements

---
+++ src/components/MultiElements.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props => 
+  <div>
+    <h1>Part I</h1>
+    <h2>Part II</h2>
+  </div>
+  
+// export default props => 
+//   <React.Fragment>
+//     <h1>Part I</h1>
+//     <h2>Part II</h2>
+//   </React.Fragment>
+
+// export default props => 
+//   [
+//     <h1>Part I</h1>,
+//     <h2>Part II</h2>
+//   ]
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import FirstComponent from './components/FirstComponent';
 import { CompA, CompB } from './components/TwoComponents';
+import Elements from './components/MultiElements';
 
 const root = document.getElementById('root');
 
@@ -10,7 +11,7 @@ ReactDom.render(<h1>Hello, React!</h1>, root);
 ReactDom.render(
   <div>
     <FirstComponent value="First component with React!!"/>
-
+    <Elements />
     <CompA value="Yeah, I'm A!" />
     <CompB value="B on area, save!" />
   </div>
